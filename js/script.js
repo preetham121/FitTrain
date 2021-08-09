8174d7c4f83a436d8e09d74641667ee10d85a143
Preetham Kotian <preetham.k.1211@gmail.com>
2021-08-09 21:39:29 +0530

    first commit

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ const showcase = document.querySelector(".showcase");
 const line1 = document.querySelector(".line1");
 const line2 = document.querySelector(".line2");
 const line3 = document.querySelector(".line3");
+const signIn = document.querySelector(".signIn");
+const signUp = document.querySelector(".signup");
 
 burger.addEventListener("click", () => {
   navLinks.classList.toggle("nav-links-visible");
@@ -11,4 +13,12 @@ burger.addEventListener("click", () => {
   line2.classList.toggle("cross-mark2");
   line1.classList.toggle("cross-mark1");
   line3.classList.toggle("cross-mark3");
+});
+
+signIn.addEventListener("click", () => {
+  alert("Sucessfully Signed In\nThank you for using");
+});
+
+signUp.addEventListener("click", () => {
+  alert("Sucessfully Signed Up\nThank you for Enrolling");
 });
